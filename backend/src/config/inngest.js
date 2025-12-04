22326e0abc1e270a2868f06df2d63120e27fcc7e
Hirwa Tresor <waveb6133@gmail.com>
2025-12-05 00:23:29 +0200

    minor backend fix

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import connectDB from "./db.js";
+import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
 
 export const inngest = new Inngest({ id: "ecommerce-app" });
@@ -10,6 +10,7 @@ const syncUser = inngest.createFunction(
   async ({ event }) => {
     await connectDB();
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
@@ -24,7 +25,7 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user" },
+  { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     await connectDB();
