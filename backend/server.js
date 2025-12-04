216677583f9ec182514b063cf73259abb304022b
Hirwa Tresor <waveb6133@gmail.com>
2025-12-05 00:34:39 +0200

    minor backend fix

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
-app.use("/api/inngest", serve({ client: inngest, functions, signingKey: ENV.INNGEST_SIGNING_KEY, }));
+app.use("/api/inngest", serve({ client: inngest, functions, signingKey: ENV.INNGEST_SIGNIN_KEY, }));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
